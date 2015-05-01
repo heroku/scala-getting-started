627854b9aabb3ffb70c3512f9e2d968aa6eefc4e
Joe Kutner <jpkutner@gmail.com>
2015-05-01 14:16:45 -0500

    Added db

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,9 +10,31 @@ import javax.measure.quantity.Mass
 import org.jscience.physics.model.RelativisticModel
 import org.jscience.physics.amount.Amount
 
+import play.api.db._
+
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def db = Action {
+    var out = ""
+    val conn = DB.getConnection()
+    try {
+      val stmt = conn.createStatement
+
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
+
+      val rs = stmt.executeQuery("SELECT tick FROM ticks")
+
+      while (rs.next) {
+        out += "Read from DB: " + rs.getTimestamp("tick") + "\n"
+      }
+    } finally {
+      conn.close()
+    }
+    Ok(out)
   }
 }
--- build.sbt
+++ build.sbt
@@ -11,7 +11,8 @@ libraryDependencies ++= Seq(
   anorm,
   cache,
   ws,
-  "org.jscience" % "jscience"   % "4.3.1"
+  "org.jscience" % "jscience"   % "4.3.1",
+  "org.postgresql" % "postgresql" % "9.4-1201-jdbc4"
 )
 
 libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _ )
