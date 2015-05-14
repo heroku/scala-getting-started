5647db5b29588f36c0f7c95d9f27257b6c7f1bda
Jon Mountjoy <tech@jonmountjoy.com>
2015-05-14 12:31:49 +0100

    tidy up local changes

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,11 +5,6 @@ import play.api.mvc._
 import play.api.cache.Cache
 import play.api.Play.current
 
-import javax.measure.unit.SI.KILOGRAM
-import javax.measure.quantity.Mass
-import org.jscience.physics.model.RelativisticModel
-import org.jscience.physics.amount.Amount
-
 import play.api.db._
 
 object Application extends Controller {
